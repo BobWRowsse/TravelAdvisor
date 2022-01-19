3096ce5b0ee5d7ba5a902024ebc69899de6682ce
BobWRowsse <bobrowsse.walakira@hyperisland.se>
2022-01-19 14:02:12 +0100

    dependancies setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import { Header, Map, PlaceDetails, List } from "./components";
 
 const App = () => {
   return (
-    <div className=''>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} className='w-full'>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
